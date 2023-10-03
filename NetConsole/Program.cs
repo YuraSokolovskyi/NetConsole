7126efa0acc022a0a3f73ee63b7b63af085948c2
YuraSokolovskyi <sokolovskiiyura@gmail.com>
2023-10-03 14:00:16 -0400

    Initial commit

---
+++ NetConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Console App");
+        }
+    }
+}
